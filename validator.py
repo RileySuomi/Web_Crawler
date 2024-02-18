94e53c3f62d1f3c73dfb5cec02f7faaa46407b1d
DANNY130 <102614682+DANNY130@users.noreply.github.com>
2024-02-17 18:01:24 -0800

    initial structure for project

---
+++ validator.py
@@ -0,0 +1,23 @@
+import validators # library that holds validators for various inputs
+
+# function that validates a URL
+def validate_url(url):
+    if validators.url(url):
+        return True
+    else:
+        return False
+
+# function to validate the given depth (must be greater than 0)
+def validate_depth(depth) :
+    if depth <= 0:
+        return -1
+    
+
+
+def main():
+    with open('URL_List.txt','r') as file:
+        pass
+    pass
+
+if __name__ == "main":
+    main()
