1d5b626c2e44215c4e8a138ce93bab9545645820
RileySuomi <suomir@spu.edu>
2024-02-29 11:20:45 -0800

    updating algorithm practice and install-list

--- algorithms.py
+++ algorithms.py
@@ -1,12 +1,13 @@
 
 #Algorithms examples
 
-import algorithms
+import algorithms #library that implements a LOTT
 
-from algorithms.sort import bogo_sort as bg
+from algorithms.sort import bogo_sort as bg # example using bogo 
 
 if __name__ == "__main__":
     my_list = [1, 8, 3, 5, 6]
     my_list = bg(my_list)
     print(my_list)
+
 
