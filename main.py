c7a2af21e4df79b0613b88dff2cf4dda02ec77a7
RileySuomi <suomir@spu.edu>
2024-02-29 11:58:58 -0800

    succesfully creating the graph.. need to display still

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import sys
 from bs4 import BeautifulSoup
 import requests
 import networkx as nx
@@ -16,25 +16,27 @@ class GraphVertex:
 
 def crawl(url, depth): 
 
-    graph = Digraph() # instantiate the graph
+    # instantiate the graph
+    graph = nx.DiGraph() 
 
     # want to create a queue that orders through the hrefs from the starter link 
     queue = [(url, 0)] 
 
     while queue: #queue not empty
-        curr_url = queue.pop(0) 
-        curr_depth = 0
+
+        # get the current url and current depth
+        curr_url, curr_depth = queue.pop(0) 
 
         # need some conditions based on depth and if valid
-        if curr_depth > depth and validator.valid_url(next_url) == False: 
+        if curr_depth > depth or not validator.valid_url(curr_url): 
             continue 
 
         try: 
             res = requests.get(curr_url) # fetch the page
 
             if res.status_code == 200: # code 200 represents a valide url 
-
-                soup = BeautifulSoup(res.content, 'html parser') # parse the current html
+                print(f"crawling {curr_url} ...")
+                soup = BeautifulSoup(res.content, 'html.parser') # parse the current html
 
                 # find all links on the current html (a list). 
                 # 'a' represents the <a tag in html which is used for hyperlinks, then also make sure it is a href
@@ -47,19 +49,32 @@ def crawl(url, depth):
                     # check if valid html, some are different
                     if not validator.valid_url(next_url): # page.html
                         continue 
-                    else:
-                        graph.add_edge(curr_url, next_url)
+
+                    #if next url is valid, we want to add it to queue 
+                    # as well as increase the depth since we are going to make a jump here
+                    queue.append((next_url, curr_depth + 1))
+
+                    # add an edge from current url to here
+                    graph.add_edge(curr_url, next_url)
 
         
-        except Exception as here: 
-             print(f"Error crawling {curr_url}: {here}")
+        except Exception as err: # error handler
+             print(f"Error crawling {curr_url}: {err}")
 
-
-
+    # want to return the graph created
+    return graph
 
 def main():
 
     pass
 
-if __name__ == "main":
-    main()
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 3:
+        print("Incorrect amount of flags")
+
+    # command line process should be "python <url> <depth>"
+    url = sys.argv[1]
+    depth = int(sys.argv[2])
+
+    graph = crawl(url, depth)
